aac982a469b49d44ffbadfbd3f28a809471452ed
James Slater <jslater@freegeekchicago.org>
2013-10-12 18:10:14 -0500

    Fix issues with commenting out source repos and adding update repos

--- install.sh
+++ install.sh
@@ -19,21 +19,38 @@ echo "################################"
 # <releasename>-security main restricted universe multiverse
 # <releasename>-updates main restricted
 
-echo "#  Commenting out source repositories -- we don't mirror them locally."
-sed -i 's/deb-src/#deb-src/' /etc/apt/sources.list
+### Disable Source Repos
+#
+# Check to see if Source repos are set ON and turn OFF
+if grep -q " deb-src" /etc/apt/sources.list; then
+    echo "# Commenting out source repositories -- we don't mirror them locally."
+    sed -i 's/ deb-src/#deb-src/' /etc/apt/sources.list
+else
+    echo "# Already disabled source repositories"
+fi
 
 # Figure out if this part of the script has been run already
 # TODO: Look at the default sources.list
-grep "${DISTRIB_CODENAME}-updates universe" /etc/apt/sources.list
-if (($? == 1)); then
-        echo "Adding ${DISTRIB_CODENAME} updates line for universe and multiverse"
-        cp /etc/apt/sources.list /etc/apt/sources.list.backup
-        echo "deb http://us.archive.ubuntu.com/ubuntu/ ${DISTRIB_CODENAME}-updates universe multiverse" >> /etc/apt/sources.list
+#grep "${DISTRIB_CODENAME}-updates universe" /etc/apt/sources.list
+#if (($? == 1)); then
+#    echo "* Adding ${DISTRIB_CODENAME} updates line for universe and multiverse"
+#    cp /etc/apt/sources.list /etc/apt/sources.list.backup
+#    echo "deb http://us.archive.ubuntu.com/ubuntu/ ${DISTRIB_CODENAME}-updates universe multiverse" >> /etc/apt/sources.list
+#else
+#    echo "# Already added universe and multiverse ${DISTRIB_CODENAME}-updates line to sources,"
+#fi
+
+# Figure out if this part of the script has been run already
+if grep -q "${DISTRIB_CODENAME}-updates universe" /etc/apt/sources.list; then
+    echo "# Already added universe and multiverse ${DISTRIB_CODENAME}-updates line to sources,"
 else
-        echo "#  Already added universe and multiverse ${DISTRIB_CODENAME}-updates line to sources,"
+    echo "* Adding ${DISTRIB_CODENAME} updates line for universe and multiverse"
+    cp /etc/apt/sources.list /etc/apt/sources.list.backup
+    echo "deb http://us.archive.ubuntu.com/ubuntu/ ${DISTRIB_CODENAME}-updates universe multiverse" >> /etc/apt/sources.list
 fi
 
-## Enable Medibuntu Repos
+### Enable Medibuntu Repos
+#
 # Commented out, no longer maintained.
 #if [ -e /etc/apt/sources.list.d/medibuntu.list ]; then
 #        echo "#  Already added Medibuntu repo, OK."
@@ -42,18 +59,22 @@ fi
 #        wget http://www.medibuntu.org/sources.list.d/${DISTRIB_CODENAME}.list -O /etc/apt/sources.list.d/medibuntu.list
 #fi
 
-## Disable and Remove Any Medibuntu Repos
+
+### Disable and Remove Any Medibuntu Repos
+#
 if [ -e /etc/apt/sources.list.d/medibuntu.list ]; then
-    echo "# Removing Medibuntu Repos."
+    echo "* Removing Medibuntu Repos."
     rm /etc/apt/sources.list.d/medibuntu*
 else
-    echo "# Medibuntu Repos Have Already Been Removed."
+    echo "# Already removed Medibuntu's libdvdcss repo."
 fi
 
-# Enable VideoLAN's libdvdcss repo
+### Enable VideoLAN Repo for libdvdcss
+#
 if [ -e /etc/apt/sources.list.d/videolan.sources.list ]; then
-        echo "#  Already added libdvdcss repo, OK."
+    echo "# Already added libdvdcss repo, OK."
 else
+    echo "* Adding VideoLAN's libdvdcss repo, OK."
 	echo 'deb http://download.videolan.org/pub/debian/stable/ /' >> /etc/apt/sources.list.d/videolan.sources.list
 #       echo 'deb-src http://download.videolan.org/pub/debian/stable/ /' >> /etc/apt/sources.list.d/videolan.sources.list
 	wget -O - http://download.videolan.org/pub/debian/videolan-apt.asc|sudo apt-key add - libdvdcss
@@ -61,6 +82,7 @@ fi
 
 
 ### Enable Wine Repo
+#
 # Commented out, no longer maintained. See http://www.winehq.org/download/ubuntu
 #if [ -e /etc/apt/sources.list.d/winehq.list ]; then
 #        echo '#  Already added Wine repo, OK.'
@@ -70,8 +92,9 @@ fi
 #fi
 
 ### Enable Wine PPA
+#
 if [ -e /etc/apt/sources.list.d/ubuntu-wine-ppa-${DISTRIB_CODENAME}.list ]; then
-        echo '#  Already added Wine PPA, OK.'
+        echo '# Already added Wine PPA, OK.'
 else
         if [ $DISTRIB_MAJOR_RELEASE -ge 11 ]; then
                 add-apt-repository -y ppa:ubuntu-wine/ppa # Do this if Ubuntu 11.04 or higher
@@ -80,10 +103,12 @@ else
         fi
 fi
 
-# Update everything
+### Update everything
+#
 apt-get -y update && apt-get -y upgrade
 
 ### Install FreeGeek's default packages
+#
 # Add codecs / plugins that most people want
 apt-get -y install ubuntu-restricted-extras totem-mozilla libdvdcss2 non-free-codecs
 apt-get -y install ttf-mgopen gcj-jre ca-certificates vlc mplayer chromium-browser
@@ -101,9 +126,11 @@ else
 fi
 
 ### Remove conflicting default packages
+#
 apt-get -y remove gnumeric* abiword*
 
-# Ensure installation completed without errors
+### Ensure installation completed without errors
+#
 apt-get -y install sl
 echo "Installation complete -- relax, and watch this STEAM LOCOMOTIVE"
 if [ $DISTRIB_MAJOR_RELEASE -ge 10 ]; then
