13423c5e560f0fe342c69e04424ed2755a8cc879
James Slater <jslater@freegeekchicago.org>
2015-03-08 09:37:35 -0500

    Initial commit of MacBook script

---
+++ apple_ubuntu.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+## Define Variables
+MODEL=`dmidecode -s system-product-name`
+MACBOOK21='MacBook2,1'
+MACBOOK41='MacBook4,1'
+#~/.config/xfce4/xfconf/xfce-perchannel-xml/thunar-volman.xml
+
+## Install webcam firmware
+webcam() {
+    apt-get install cheese
+    apt-get install isight-firmware-tools
+    wget -P /lib/firmware http://fgc-nfs/iSight/AppleUSBVideoSupport
+    ift-extract -a /lib/firmware/AppleUSBVideoSupport
+}
+
+### Install Broadcom Wireless Driver
+wireless() {
+    apt-get install firmware-b43-installer b43-fwcutter
+}
+
+## Fix touchpad issues
+touchpad() {
+### Current session
+    synclient FingerLow=10
+    synclient FingerHigh=20
+### Permanently
+    if [ ! -d "/etc/X11/xorg.conf.d" ]; then
+        mkdir /etc/X11/xorg.conf.d
+    fi
+ 
+    echo 'Section "InputClass"
+    Identifier "touchpad catchall"
+    MatchIsTouchpad "on"
+    MatchDevicePath "/dev/input/event*"
+    Driver "synaptics"
+    Option "FingerLow" "10"
+    Option "FingerHigh" "20"
+EndSection' >> /etc/X11/xorg.conf.d/10-synaptics.conf
+}
+
+## MACBOOK2,1
+if [ $MODEL = $MACBOOK21 ]; then
+    echo "You are using an Apple $MODEL."
+    echo "Installing iSight webcam"
+    webcam
+    echo "Installing wireless drivers"
+    wireless
+    echo "Fixing touchpad sensitivity"
+    touchpad
+fi
+
+## MACBOOK4,1
+if [ $MODEL = $MACBOOK41 ]; then
+    echo "You are using an Apple $MODEL."
+    echo "Installing iSight webcam"
+    webcam
+    echo "Installing wireless drivers"
+    wireless
+    echo "Fixing touchpad sensitivity"
+    touchpad
+fi
+
