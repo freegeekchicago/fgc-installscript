d86da8dab54f8f46cf3c037ac83b6bf7300c17e3
James Slater <jslater@freegeekchicago.org>
2015-03-13 14:26:40 -0500

    Change to double quotes

--- install.sh
+++ install.sh
@@ -246,7 +246,7 @@ apt-get -y install linux-firmware-nonfree
 ###################################
 
 MANUFACTURER=`dmidecode -s system-manufacturer`
-if [ '$MANUFACTURER' = 'Apple Inc.' ]; then
+if [ "$MANUFACTURER" = "Apple Inc." ]; then
     echo "You are using an $MANUFACTURER."
 
     # Remove current apple_ubuntu.sh
@@ -261,7 +261,7 @@ if [ '$MANUFACTURER' = 'Apple Inc.' ]; then
 
     # Run install.sh for updates
     echo "## Running apple_ubuntu.sh, BYE!"
-    sh /usr/local/bin/apple_ubunt.sh
+    . /usr/local/bin/apple_ubuntu.sh
 fi
 
 #################################
