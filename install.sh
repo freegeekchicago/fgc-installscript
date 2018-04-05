a750367ea24718997bdaed488c952a6244cc3ed3
Alex Hanson <ahanson@runbox.com>
2018-04-05 17:48:11 -0500

    Update install.sh
    Removed 12.04 section.

--- install.sh
+++ install.sh
@@ -193,23 +193,6 @@ if [ $(lsb_release -rs) = '14.04' ]; then
 	fi
 fi
 
-###
-### Packages for Precise (12.04) ###
-####################################
-
-if [ $(lsb_release -rs) = '12.04' ]; then
-    echo "* Customizing Precise packages."
-    apt-get -y install ttf-mgopen
-
-	# Xubuntu 12.04 Specific Packages
-	if [ $(dpkg-query -W -f='${Status}' xubuntu-desktop 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
-	    echo "* Customizing Precise-Xubuntu packages."
-	    apt-get -y install xubuntu-restricted-extras
-	    apt-get -y remove gnumeric* abiword* dragonplayer
-	fi
-fi
-
-
 ###############
 ### Packages for All Releases
 ###############
